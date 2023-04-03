21f3d3e15140582d412b2ea90e2a02e2d5feb69e
unknown <wixproyecto2@gmail.com>
2023-04-03 14:41:37 -0600

    first commit 24

--- routes/usersadmin.js
+++ routes/usersadmin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const validate = require('../validacion');
+const { validateUser } = require('../validacion');
 
 router.get('/users', async (req, res, next) => {
 	try {
@@ -28,16 +28,12 @@ router.get('/users/:id', async (req, res, next) => {
 	}
 });
 
-router.post('/users', async (req, res, next) => {
+router.post('/users', validateUser, async (req, res, next) => {
 	try {
-		const data = req.body;
-		const errors = validate(data); // Validating user input
-		if (errors.length) {
-			return res.status(400).json({ errors }); // If there are errors, return them
-		}
+		const { name, email } = req.body;
 		await pool.execute(
 			'INSERT INTO users (nombre, correo) VALUES (?, ?)',
-			[data.name, data.email]
+			[name, email]
 		);
 		res.json({ message: 'User created successfully' });
 	} catch (error) {
@@ -48,14 +44,10 @@ router.post('/users', async (req, res, next) => {
 router.put('/users/:id', async (req, res, next) => {
 	try {
 		const { id } = req.params;
-		const data = req.body;
-		const errors = validate(data); // Validating user input
-		if (errors.length) {
-			return res.status(400).json({ errors }); // If there are errors, return them
-		}
+		const { name, email } = req.body;
 		await pool.execute(
 			'UPDATE users SET nombre = ?, correo = ? WHERE id = ?',
-			[data.name, data.email, id]
+			[name, email, id]
 		);
 		res.json({ message: 'User updated successfully' });
 	} catch (error) {
