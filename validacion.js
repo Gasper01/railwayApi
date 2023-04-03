5d1e57bc1a9341d08e450d97d13df203a0a93585
unknown <wixproyecto2@gmail.com>
2023-04-03 14:37:42 -0600

    first commit 23

--- validacion.js
+++ validacion.js
@@ -1,16 +1,13 @@
-const { validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-const validate = (validations) => async (req, res, next) => {
-	await Promise.all(
-		validations.map((validation) => validation.run(req))
-	);
-
-	const errors = validationResult(req);
-	if (errors.isEmpty()) {
-		return next();
-	}
-
-	res.status(400).json({ errors: errors.array() });
-};
-
-module.exports = validate;
+exports.validateUser = [
+	body('name').notEmpty().withMessage('Name is required'),
+	body('email').isEmail().withMessage('Email must be valid'),
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		next();
+	},
+];
